5ce993a2d02e64101742c5ec08e2383d13130b90
Vedraj Mokashi <vedraj.mokashi@gmail.com>
2025-11-23 02:04:19 +0530

    Added chat conversation style UI changes

--- youtube-rag-chatbot/frontend/my-ui/src/app/components/chat/chat.ts
+++ youtube-rag-chatbot/frontend/my-ui/src/app/components/chat/chat.ts
@@ -1,8 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, output, signal } from '@angular/core';
+import { Component, ElementRef, input, output, signal, viewChild, ViewChild, AfterViewChecked } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Api } from '../../services/api';
 import { AskRequest, AskResponse } from '../../models/api-models';
+
+// Chat Message Interface
+interface ChatMessage{
+  type : 'user' | 'assistant';
+  content : string;
+  timestamp : Date;
+}
 
 @Component({
   selector: 'app-chat',
@@ -14,14 +21,29 @@ import { AskRequest, AskResponse } from '../../models/api-models';
 export class Chat {
   videoId = input.required<string>();
   question = signal('');
-  answer = signal('');
+  messages = signal<ChatMessage[]>([]); //for chat messages array
   isAsking = signal(false);
   error = signal('');
 
   processNewVideo = output<void>() // Emit event when user wants new video
   
+  //viewChild() is the Angular-safe way to access the DOM
+  private chatContainer = viewChild<ElementRef>('chatContainer')
 
   constructor(private apiService : Api) {}
+
+  //Auto scroll life cycle hook
+  ngAfterViewChecked() : void{
+    this.scrollToBottom();
+  }
+
+  //auto scrolling logic
+  private scrollToBottom() : void {
+    const container = this.chatContainer()?.nativeElement;
+    if(container){
+      container.scrollTop = container.scrollHeight
+    }
+  }
 
   askQuestion() : void{
     
@@ -34,9 +56,12 @@ export class Chat {
       this.error.set("Please enter a Question");
       return;
     }
+    const userQuestion = this.question().trim()
 
+    //adds user message to chat history
+    this.addMessage('user', userQuestion);
     this.isAsking.set(true);
-    this.answer.set('');
+    // this.answer.set('');
     this.error.set('');
 
     const request : AskRequest = {
@@ -46,7 +71,9 @@ export class Chat {
 
     this.apiService.askQuestion(request).subscribe({
       next : (response : AskResponse) => {
-        this.answer.set(response.answer);
+        //Add assistant message to chat history
+        this.addMessage('assistant', response.answer);
+        // this.answer.set(response.answer);
         this.isAsking.set(false);
         console.log('API Response : ', response);
       },
@@ -58,7 +85,18 @@ export class Chat {
     })
   }
 
+  private addMessage(type : 'user' | 'assistant', content : string) : void{
+    this.messages.update(messages => [
+      ...messages,
+      {type, content, timestamp : new Date() }
+    ]);
+  }
+
   newVideo() : void{
+    //clear chat history on new video
+    this.messages.set([]);
+    this.question.set('');
+    this.error.set('')
     this.processNewVideo.emit();
   }
 
